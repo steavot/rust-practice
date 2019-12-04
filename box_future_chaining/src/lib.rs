5d5c2a17db93ed8f509d8c36d8c7e4b48e648224
tarquin-the-brave <tomsteavenson@gmail.com>
2019-12-04 05:17:27 +0000

    fix box dyn futures example with Either

--- box_future_chaining/src/lib.rs
+++ box_future_chaining/src/lib.rs
@@ -1,5 +1,5 @@
 use futures::{
-    future::{err, ok},
+    future::{ok, Either, FutureResult},
     Future,
 };
 
@@ -14,21 +14,21 @@ enum OtherResponse {
     Success,
 }
 
-fn top_level() -> Box<dyn Future<Item = MyResponse, Error = ApiError>> {
-    let x = other();
-    // .and_then(|rsp: OtherResponse| -> Box<dyn Future<Item = Result<String, MyResponse>, Error = ApiError>> {
-    //     Box::new(ok(handle_other(rsp)))
-    // });
+// fn top_level() -> Box<dyn Future<Item = MyResponse, Error = ApiError>> {
+//     let x = other();
+// .and_then(|rsp: OtherResponse| -> Box<dyn Future<Item = Result<String, MyResponse>, Error = ApiError>> {
+//     Box::new(ok(handle_other(rsp)))
+// });
 
-    // .and_then(|rsp: Result<String, MyResponse>| -> Box<dyn Future<Item = Result<String, MyResponse>, Error = ApiError>> {
-    //             match rsp {
-    //                 Err(rsp) => Box::new(ok(Err(rsp))),
-    //                 Ok(_data) => (),
-    //             }
-    //         })
-    // Box::new(x)
-    x.map(|r| Box::new(other_into_my(r)))
-}
+// .and_then(|rsp: Result<String, MyResponse>| -> Box<dyn Future<Item = Result<String, MyResponse>, Error = ApiError>> {
+//             match rsp {
+//                 Err(rsp) => Box::new(ok(Err(rsp))),
+//                 Ok(_data) => (),
+//             }
+//         })
+// Box::new(x)
+//     x.map(|r| Box::new(other_into_my(r)))
+// }
 
 fn other() -> Box<dyn Future<Item = OtherResponse, Error = ApiError>> {
     Box::new(ok(OtherResponse::Success))
@@ -46,4 +46,29 @@ fn other_into_res(rsp: OtherResponse) -> Result<String, MyResponse> {
         OtherResponse::A | OtherResponse::B => Err(MyResponse::C),
         OtherResponse::Success => Ok("success".to_string()),
     }
+}
+
+fn using_either() -> Box<dyn Future<Item = MyResponse, Error = ApiError>> {
+    Box::new(
+        other()
+            .and_then(
+                |rsp: OtherResponse| -> FutureResult<Result<String, MyResponse>, ApiError> {
+                    ok(other_into_res(rsp))
+                },
+            )
+            .and_then(|rsp: Result<String, MyResponse>| -> Either<_, _> {
+                // Item=Result<String, MyResponse>
+                // Error=ApiError
+                match rsp {
+                    Err(r) => Either::A(ok(Err(r))),
+                    Ok(_data) => Either::B(other().and_then(|r| ok(other_into_res(r)))),
+                }
+            })
+            .and_then(|rsp: Result<String, MyResponse>| -> Either<_, _> {
+                match rsp {
+                    Err(r) => Either::A(ok(r)),
+                    Ok(_data) => Either::B(other().and_then(|r| ok(other_into_my(r)))),
+                }
+            }),
+    )
 }
