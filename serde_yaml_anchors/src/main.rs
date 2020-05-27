b1e73be76ca686b3ab3ea8ba4cee961d837f6725
tarquin-the-brave <tomsteavenson@gmail.com>
2020-05-27 14:03:46 +0100

    update to example used in serde_yaml issue

--- serde_yaml_anchors/src/main.rs
+++ serde_yaml_anchors/src/main.rs
@@ -12,20 +12,18 @@ struct OtherData {
     town: String,
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() {
     // One liner, useful in future.
-    // let typed = serde_yaml::from_reader::<std::io::Stdin, SomeData>(std::io::stdin());
+    let untyped = serde_yaml::from_reader::<std::io::Stdin, serde_yaml::Value>(std::io::stdin());
 
-    let mut input = String::new();
-    std::io::Read::read_to_string(&mut std::io::stdin(), &mut input)?;
+    // let mut input = String::new();
+    // std::io::Read::read_to_string(&mut std::io::stdin(), &mut input)?;
 
     // let typed = serde_yaml::from_str::<SomeData>(&input)?;
-    let untyped = serde_yaml::from_str::<serde_yaml::Value>(&input)?;
+    // let untyped = serde_yaml::from_str::<serde_yaml::Value>(&input)?;
 
     // println!("\ntyped read as:");
     // println!("{:?}", typed);
     println!("\nuntyped read as:");
     println!("{:?}", untyped);
-
-    Ok(())
 }
